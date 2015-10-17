ec46b9dfdc6d3d2584a8b6192dfad8a55dc5a78f
Saad Ahmed <saadem3000@gmail.com>
2015-10-17 08:56:21 -0400

    Random Commit

--- UWindsorMSA-iOSApp/AboutViewController.swift
+++ UWindsorMSA-iOSApp/AboutViewController.swift
@@ -44,11 +44,6 @@ class AboutViewController: UIViewController {
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
 
 
